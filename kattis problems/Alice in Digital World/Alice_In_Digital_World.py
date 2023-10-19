27c565b5fd6301cc5ce53c8db213b25d3b767a76
Mr-Nnobody <124190316+Mr-Nnobody@users.noreply.github.com>
2023-10-19 10:05:49 +0100

    Update Alice_In_Digital_World.py

--- kattis problems/Alice in Digital World/Alice_In_Digital_World.py
+++ kattis problems/Alice in Digital World/Alice_In_Digital_World.py
@@ -1,5 +1,5 @@
 import sys
-
+#reading standard inputs 
 test_cases = int(sys.stdin.readline())
 
 while test_cases:
@@ -13,26 +13,28 @@ while test_cases:
     m_idx = 0
 
     idx = 0
-#looping through Array Numbers and getting the submay
+#looping through Array Numbers and getting the subarray
     while idx < n:
         number = numbers[idx]
-
+        #putting aside any number which is less than oour minimum number
         if number < m:
             running_weight = 0
             m_seen = False
             idx += 1
             continue
-
+        #finds the minimum number
         if number == m:
+            #makes sure only one minimum number is present in Subarray
             if m_seen:
                 m_seen = False
                 idx = m_idx + 1
                 running_weight = 0
                 continue
+            #indicates that minimum number has been found
             else:
                 m_seen = True
                 m_idx = idx
-
+        #computes the weighted sum of easch subarray and returns the maximum
         running_weight = running_weight + number
         if m_seen:
             max_weight = max(running_weight, max_weight)
