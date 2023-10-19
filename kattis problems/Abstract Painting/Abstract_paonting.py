16f19a1f439587a666933d1fa5dde1deda5be503
Mr-Nnobody <124190316+Mr-Nnobody@users.noreply.github.com>
2023-10-19 09:57:55 +0100

    Update Abstract_painting.py
    i

--- kattis problems/Abstract Painting/Abstract_paonting.py
+++ kattis problems/Abstract Painting/Abstract_paonting.py
@@ -1,11 +1,11 @@
 import sys
-
+# reading standard inputs
 test_cases = int(sys.stdin.readline())
 
 while test_cases:
     r, c = map(int, sys.stdin.readline().split())
 
-
+#getting number of arrangments ensuring its <= MOD
     MOD = 10**9+7
     print(pow(3, r+c, MOD)*pow(2, r*c, MOD)%MOD)
     test_cases -= 1
